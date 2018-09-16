51027473ad68eece9e8c3e5310b73285df07e75f
chan328 <rkdcks020328@gmail.com>
2018-09-16 23:10:22 +0900

    합체

---
+++ Assets/Scripts/Monster/MonsterLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterLogic : MonoBehaviour {
+    public GameObject Player;
+    public bool incount;
+    SpriteRenderer renderer;
+    protected const int ICOUNT_LEGNTH = 5;
+    protected bool left { get; set; } // 벽에 부딫혔을 때 이동방향을 정해줌.
+               // Use this for initialization
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        left = false;
+    }
+    
+    protected void Move() // 순찰
+    {
+        if (left == true)
+        {
+            renderer.flipX = true;
+            transform.Translate(Vector2.left * 0.1f);
+        }
+        else
+        {
+            renderer.flipX = false;
+            transform.Translate(Vector2.right * 0.1f);
+        }
+    }
+
+    protected bool getLeft()
+    {
+        return left;
+    }
+    
+}
---
+++ Assets/Scripts/UI/Stage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stage : MonoBehaviour {
+    public GameObject CurrentStage;
+    public Text StageText;
+    private string StageInfo;
+	// Use this for initialization
+	void Start () {
+        StageInfo = CurrentStage.GetComponent<ExampleStage>().currentStage; // ExamplStage에 있는 현재 스테이지 string값을 가져온다.
+        StageText.text = "Stage : " + StageInfo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
