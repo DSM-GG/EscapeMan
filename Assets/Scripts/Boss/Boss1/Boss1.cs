1db72ae2888a60e60dd61c7b24b4e7de53bcd116
PSDent <mnsan1004@gmail.com>
2018-11-11 15:28:03 +0900

    2018-11-11 / 보스1 이동 구현, 폭탄, 미사일 구현중

--- Assets/Scripts/Boss/Boss1/Boss1.cs
+++ Assets/Scripts/Boss/Boss1/Boss1.cs
@@ -7,18 +7,34 @@ public class Boss1 : MonsterLogic2 {
     public float floatSpeed;
     public float moveSpeed;
 
+    public float moveCoolTime;
+    public float bombCoolTime;
+    public float missileCoolTime;
+
+    WaitForSeconds wfs_missile, wfs_moving, wfs_bomb;
+
     Vector3 scale;
-    Vector3 AdditionPos;
-    Vector3 MovingPos;
+    Vector3 additionPos;
+    Vector3 movingPos;
     float time = 0.0f;
-    float dir;
+    int dir = 1;
+
+    float prevX = 0, nowX = 0;
+    bool isMovingCool, isMissileCool, isBombCool;
 
 	// Use this for initialization
 	void Start () {
-        AdditionPos = new Vector3(0, 0, 0);
+        additionPos = new Vector3(0, 0, 0);
         scale = new Vector3();
-        MovingPos = new Vector3();
+        movingPos = new Vector3();
         player = GameObject.Find("RockMan");
+
+        wfs_missile = new WaitForSeconds(missileCoolTime);
+        wfs_moving = new WaitForSeconds(moveCoolTime);
+        wfs_bomb = new WaitForSeconds(bombCoolTime);
+
+        nowX = transform.position.x;
+        isMovingCool = isMissileCool = isBombCool = false;
 
         Flip();
     }
@@ -36,21 +52,6 @@ public class Boss1 : MonsterLogic2 {
 
     protected override void Check()
     {
-        Rincount = transform.GetChild(0).GetChild(0).gameObject.GetComponent<Observer>().Incount;
-        Lincount = transform.GetChild(0).GetChild(1).gameObject.GetComponent<Observer>().Incount;
-
-        if (Rincount == true || Lincount == true)
-            isIncount = true;
-        else
-            isIncount = false;
-
-        if (player.transform.position.x > transform.position.x)
-            playerIsLeft = false;
-        else
-            playerIsLeft = true;
-
-        //if (monsterHp <= 0)
-        //    StartCoroutine("MonsterDying");
     }
 
     // 둥둥 떠다니는 이동을 위함
@@ -59,19 +60,47 @@ public class Boss1 : MonsterLogic2 {
         time += Time.deltaTime * floatSpeed;
         float y = Mathf.Sin(time) * 0.02f;
         Debug.Log(y);
-        AdditionPos.Set(0, y, 0);
-        transform.position += AdditionPos;
+        additionPos.Set(0, y, 0);
+        transform.position += additionPos;
     }
 
     void Moving()
     {
+        if (isMovingCool) return;
+        nowX = transform.position.x;
 
+        if(prevX == nowX)
+        {
+            StartCoroutine("MovingCool");
+        }
+
+        movingPos.Set(moveSpeed * dir, 0, 0);
+        transform.position += movingPos;
+        prevX = nowX;
     }
-
+        
     void Flip()
     {
         scale.Set(transform.localScale.x * -1, 1, 1);
-        dir = transform.localScale.x;
         transform.localScale = scale;
+        dir = (int)transform.localScale.x;
+    }
+
+    IEnumerator MovingCool()
+    {
+        Flip();
+        isMovingCool = true;
+        yield return wfs_moving;
+        isMovingCool = false;
+    }
+
+    IEnumerator BombCool()
+    {
+        yield return wfs_bomb;
+    }
+
+    IEnumerator MissileCool()
+    {
+        yield return wfs_missile;
     }
 }
